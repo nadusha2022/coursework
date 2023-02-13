180a2a49e33cadede52dbadd878eb9f3097bb9d5
a1 <nadyagulikyan@gmail.com>
2023-02-13 21:38:12 +0500

    CR

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+public class Main {
+    private static final Employee[] EMPLOYEES = new Employee[10];
+
+    public static void main(String[] args) {
+        fillEmployees ();
+        printFullInfo ();
+        System.out.println("Сумма ЗП " + totalSalaries());
+        System.out.println("Сотрудник с мин ЗП " + findEmployeeWithMinSalary());
+        System.out.println("Сотрудник с макс ЗП " + findEmployeeWithMaxSalary());
+        System.out.println("Средняя ЗП " + fingAvarageSalary());
+        printFullNameInfo();
+    }
+
+    private static void fillEmployees() {
+        Random random = new Random();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            EMPLOYEES[i] = new Employee("Сотрудник " + (i + 1),
+                    random.nextInt(1, 6),
+                    random.nextInt(56_000, 100_000)
+            );
+        }
+    }
+
+    private static void printFullInfo() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee);
+        }
+    }
+
+    private static int totalSalaries() {
+        int sum = 0;
+        for (Employee employee : EMPLOYEES) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    private static Employee findEmployeeWithMinSalary() {
+        int min = Integer.MAX_VALUE;
+        Employee employee = null;
+        for (Employee emp : EMPLOYEES) {
+            if (emp.getSalary() < min) {
+                min = emp.getSalary();
+                employee = emp;
+            }
+        }
+        return employee;
+    }
+
+    private static Employee findEmployeeWithMaxSalary() {
+        int max = Integer.MIN_VALUE;
+        Employee employee = null;
+        for (Employee emp : EMPLOYEES) {
+            if (emp.getSalary() > max) {
+                max = emp.getSalary();
+                employee = emp;
+            }
+        }
+        return employee;
+    }
+
+    private static double fingAvarageSalary () {
+        return totalSalaries() / (double) EMPLOYEES.length;
+    }
+
+    private static void printFullNameInfo () {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee.getFullName());
+        }
+    }
+}
